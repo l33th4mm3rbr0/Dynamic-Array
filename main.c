91411e7f212bf0eac63b0962c380ab23b79c3bf0
samih <66999949+l33th4mm3rbr0@users.noreply.github.com>
2020-08-10 12:25:49 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,96 @@
+// Une implémentation basique d'un tableau à allocation dymanique
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void showValues(int* values, int number_of_values);
+int* addValue(int* valuesList, int* number_of_values, int value);
+int* removeValue(int* valuesList, int* number_of_values, int value_to_remove);
+
+int main(int argc, char* argv[])
+{
+	srand((int)time(NULL));
+
+	char input; // stores user input
+	int number_of_values = 0;
+	int* values = NULL;
+
+	while (1)
+	{
+		input = getchar();
+		printf("%c\n", input);
+		if (input == 'q')
+			break;
+
+		switch (input)
+		{
+		case 'a': //add a value
+			values = addValue(values, &number_of_values, rand() % 51);
+			showValues(values, number_of_values);
+			break;
+		case 'r': //remove a value
+			if (number_of_values > 0)
+			{
+				values = removeValue(values, &number_of_values, rand() % number_of_values);
+				showValues(values, number_of_values);
+			}
+			else
+				printf("cannot remove inexisting value\n");
+			break;
+		case 's':
+			showValues(values, number_of_values);
+			break;
+		}
+
+	}
+
+	if (values)
+		free(values);
+	return 0;
+}
+
+void showValues(int* values, int number_of_values)
+{
+	printf("here are all the values: \n");
+	for (int i = 0; i < number_of_values;i++)
+	{
+		printf("%d) %d\n", i+1, values[i]);
+	}
+}
+
+int* addValue(int* valuesList, int* number_of_values, int value)
+{
+	*number_of_values+=1;
+	int* newList = malloc(sizeof(int)*(*number_of_values));
+
+	for (int i = 0; i < *number_of_values;i++)
+	{
+		if (i < *number_of_values - 1)
+			newList[i] = valuesList[i];
+		else
+			newList[i] = value;
+	}
+	if (valuesList)
+		free(valuesList);
+
+	return newList;
+}
+
+int* removeValue(int* valuesList, int* number_of_values, int value_to_remove)
+{
+	int* newList = malloc(sizeof(int) * (*number_of_values - 1));
+	for (int i = 0;i < *number_of_values;i++)
+	{
+		if (i < value_to_remove)
+			newList[i] = valuesList[i];
+		else if (i > value_to_remove)
+			newList[i - 1] = valuesList[i];
+	}
+	printf("removed value: %d\n", valuesList[value_to_remove]);
+	if (valuesList)
+		free(valuesList);
+	*number_of_values -= 1;
+
+	return newList;
+}
